43e14d54edb464d015528fb296cab1a6f5f3341f
Olexander <aspera180@gmail.com>
2018-02-20 23:49:12 +0200

    create Product.java

---
+++ src/main/java/com/shop/entity/Product.java
@@ -0,0 +1,78 @@
+package com.shop.entity;
+
+import com.shop.dto.product.Description;
+import com.shop.dto.product.Rating;
+import com.vladmihalcea.hibernate.type.array.IntArrayType;
+import com.vladmihalcea.hibernate.type.array.StringArrayType;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonNodeStringType;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@TypeDefs({
+        @TypeDef(name = "json", typeClass = JsonStringType.class),
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class),
+})
+@Table(name = "products")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = -7973981687713701860L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_products")
+    private Long idProduct;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "producer")
+    private String producer;
+
+    @Type(type = "json")
+    @Column(name = "description", columnDefinition = "json")
+    private List<Description> description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Type(type = "json")
+    @Column(name = "image", columnDefinition = "json")
+    private List<byte[]> listImages;
+
+    @Column(name = "isActive")
+    private Boolean isActive;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "discount")
+    private Integer discount;
+
+    @Type(type = "json")
+    @Column(name = "rating", columnDefinition = "json")
+    private List<Rating> rating;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_category", nullable = false)
+    private Category category;
+}
