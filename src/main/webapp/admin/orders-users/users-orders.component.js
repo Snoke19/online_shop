1980ca11a2b6375adc531a2c7df32156387f098b
Olexander <aspera180@gmail.com>
2018-02-21 00:41:55 +0200

    create webapp/admin/orders-users

---
+++ src/main/webapp/admin/orders-users/users-orders.component.js
@@ -0,0 +1,202 @@
+(function () {
+    'use strict';
+
+    angular.module('users-orders', [
+        'ngRoute',
+        'ngAnimate',
+        'ngTouch',
+        'vTabs',
+        'ui.grid',
+        'ui.grid.pagination',
+        'ui.grid.resizeColumns',
+        'ui.grid.moveColumns',
+        'ui.grid.selection',
+        'ui.grid.treeView',
+        'cgNotify'
+    ]);
+
+    angular.module('users-orders')
+        .component('usersOrders', {
+            templateUrl: '/admin/orders-users/users-orders.template.html',
+            controller: ['$scope', '$http', '$interval', '$timeout', 'notify', 'ngProgressFactory', 'uiGridTreeViewConstants', 'AdminService', AdminBoardUsersOrdersController]
+        });
+
+    function AdminBoardUsersOrdersController($scope, $http, $interval ,$timeout, notify, ngProgressFactory, uiGridTreeViewConstants, AdminService) {
+
+        $scope.tabs = {
+            active: 0
+        };
+        //for list
+        $scope.selectedIndex = 0;
+
+        $scope.progressbar = ngProgressFactory.createInstance();
+        $scope.progressbar.setHeight('5px');
+
+        $scope.progressbar.start();
+        AdminService.getNewOrders().then(function (d) {
+            $scope.allOrders = d;
+            $scope.progressbar.complete();
+
+            $scope.progressbar.start();
+            AdminService.getOrderItemsByIdUser($scope.allOrders[0].idOrders).then(function (d) {
+                $scope.orderUser = d;
+
+                //total price
+                $scope.pricePerQuantity = _.pluck($scope.orderUser, 'pricePerQuantity');
+                $scope.total = null;
+                angular.forEach($scope.pricePerQuantity, function(value, key){
+                    $scope.total += value;
+                });
+
+                $scope.progressbar.complete();
+
+            });
+
+            //so that the scroll is visible
+            $scope.forceOverflowUserOrder = 'scrollbar scrollbar-primary force-overflow-userOrder';
+        });
+
+        $scope.progressbar.start();
+        $scope.getOrder = function (id, index) {
+
+            //active item list
+            $scope.selectedIndex = index;
+
+            //so that the scroll is visible
+            $scope.forceOverflowUserOrder = 'scrollbar scrollbar-primary force-overflow-userOrder';
+
+            AdminService.getOrderItemsByIdUser(id).then(function (d) {
+                $scope.orderUser = d;
+
+                //total price
+                $scope.totalPrice = _.pluck($scope.orderUser, 'totalPrice');
+                $scope.total = 0.0;
+                angular.forEach($scope.totalPrice, function(value, key){
+                    $scope.total += value;
+                });
+
+                $scope.progressbar.complete();
+            });
+        };
+
+        $scope.dayAgo = function (date) {
+
+            var d = new Date();
+            d = new Date(d.getTime() - 3000000);
+            var date_format_str = d.getFullYear().toString()+"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
+
+            var tt = moment(date);
+            var yy = moment(date_format_str);
+
+            return yy.diff(tt, 'days');
+        };
+
+        $scope.progressbar.start();
+        $scope.updateNewOrderStatus = function (id) {
+
+            AdminService.updateStatusOrder('in_process', id).then(function (d) {
+
+                AdminService.getNewOrders().then(function (d) {
+                    $scope.allOrders = d;
+                });
+
+                notify({message: 'Order is admitted!', position: 'right', classes: 'alert-success'});
+                $scope.progressbar.complete();
+            })
+        };
+
+        $scope.progressbar.start();
+        $scope.cancelNewOrder = function (id) {
+
+            AdminService.updateStatusOrder('canceled', id).then(function (d) {
+
+                AdminService.getNewOrders().then(function (d) {
+                    $scope.allOrders = d;
+                });
+
+                notify({message: 'Order is canceled!', position: 'right', classes: 'alert-success'});
+                $scope.progressbar.complete();
+            })
+        };
+
+        $scope.gridOptionsOrderSItems = {
+
+            rowHeight: 140,
+
+            enableRowHashing: false,
+
+            enableRowSelection: true,
+
+            columnDefs: [
+                { field: 'product.code',
+                    displayName: '#Code',
+                    width: 90,
+                    cellClass: 'pl-1'
+                },
+                { field: 'product',
+                    displayName: 'Images',
+                    width: 165,
+                    cellTemplate: '/admin/orders-users/carouselImg.html'
+                },
+                { field: 'product.name',
+                    displayName: 'Name Product',
+                    width: 200
+                },
+                { field: 'product.price',
+                    displayName: 'Price product',
+                    width: 135
+                },
+                { field: 'quantityProducts',
+                    displayName: 'Quantity',
+                    width: 120
+                },
+                { field: 'pricePerQuantity',
+                    displayName: 'Price',
+                    width: 95
+                }
+            ],
+            data: 'orderUser'
+        };
+
+        // // AdminService.getInProcessOrders().then(function (d) {
+        // //     $scope.inProcessOrders = d;
+        // // });
+        //
+        // $scope.gridOptionsProcessingOrd = {
+        //
+        //     enableRowHashing: false,
+        //
+        //     enableRowSelection: true,
+        //
+        //     columnDefs: [
+        //         { field: 'product.code',
+        //             displayName: '#Code',
+        //             width: 90,
+        //             cellClass: 'pl-1'
+        //         },
+        //         { field: 'product',
+        //             displayName: 'Images',
+        //             width: 165,
+        //             cellTemplate: '/admin/orders-users/carouselImg.html'
+        //         },
+        //         { field: 'product.name',
+        //             displayName: 'Name Product',
+        //             width: 200
+        //         },
+        //         { field: 'product.price',
+        //             displayName: 'Price product',
+        //             width: 135
+        //         },
+        //         { field: 'quantityProducts',
+        //             displayName: 'Quantity',
+        //             width: 120
+        //         },
+        //         { field: 'pricePerQuantity',
+        //             displayName: 'Price',
+        //             width: 95
+        //         }
+        //     ],
+        //     data: 'inProcessOrders'
+        // };
+    }
+})();
